1b97fc8ee052a232f464bd85bed52b629c473f37
Anantjit Chander <anantjitchander@gmail.com>
2023-11-22 16:34:25 +0530

    Add files via upload

---
+++ Box.js
@@ -0,0 +1,11 @@
+AFRAME.registerComponent("rotate-box",{schema:{moveX:{type:"number", default:"1"},
+
+
+tick:function(){
+    this.data.moveX=this.data.moveX+5;
+    var rotate=this.el.getAttribute("rotation");
+    rotate.x=this.data.moveX;
+    rotate.y=this.data.moveX;
+
+    this.el.setAttribute("rotation",{x:rotate.x,r:rotate.y,z:rotate.z});
+}}})
